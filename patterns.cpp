4447fe506aa9e279e977c777b5b5eb9c179c892c
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-24 20:30:51 +0530

    Basic Pattern 1 completed

---
+++ patterns.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void fn()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << '*' << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    fn();
+    return 0;
+}
