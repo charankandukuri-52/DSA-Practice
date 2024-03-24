8b7bc31e3c310519a037e24b8f1b6b9f54e1adcf
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-24 21:18:19 +0530

    basic Pattern Updated

--- patterns.cpp
+++ patterns.cpp
@@ -6,9 +6,9 @@ void fn()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j <= i + 1; j++)
         {
-            cout << '*' << " ";
+            cout << j << " ";
         }
         cout << endl;
     }
